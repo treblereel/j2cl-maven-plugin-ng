001f91233dfc7b9c756ce8eef3687ce51562493f
Dmitrii Tikhomirov <chani.liet@gmail.com>
2025-11-12 21:59:18 -0800

    step 9
    Signed-off-by: Dmitrii Tikhomirov <chani.liet@gmail.com>

--- plugin/src/main/java/org/example/task/ClosureTask.java
+++ plugin/src/main/java/org/example/task/ClosureTask.java
@@ -3,7 +3,74 @@ package org.example.task;
 import org.example.context.BuildContext;
 import org.example.model.Dependency;
 
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
 public class ClosureTask extends TaskInput {
+
+    private static final Path META_INF = Paths.get("META-INF");
+    /**
+     * servlet 3 and webjars convention
+     */
+    private static final Path META_INF_RESOURCES = META_INF.resolve("resources");
+    /**
+     * optional directory to offer externs within a jar
+     */
+    private static final Path META_INF_EXTERNS = META_INF.resolve("externs");
+
+    private static final Path PUBLIC = Paths.get("public");
+
+    private static final PathMatcher JS_SOURCES = withSuffix(".js");
+
+    private static final PathMatcher XTB = withSuffix(".xtb");
+    private static final PathMatcher NATIVE_JS_SOURCES = withSuffix(".native.js");
+    private static final PathMatcher EXTERNS_SOURCES = withSuffix(".externs.js");
+
+    private static final PathMatcher IN_META_INF = path -> path.startsWith(META_INF);
+    private static final PathMatcher IN_META_INF_EXTERNS = path -> path.startsWith(META_INF_EXTERNS);
+    private static final PathMatcher IN_META_INF_RESOURCES = path -> path.startsWith(META_INF_RESOURCES);
+
+    private static final PathMatcher IN_PUBLIC = path -> StreamSupport.stream(path.spliterator(), false).anyMatch(PUBLIC::equals);
+
+    /**
+     * JS files that closure should use as type information
+     */
+    private static final PathMatcher EXTERNS = new PathMatcher() {
+        @Override
+        public boolean matches(Path path) {
+            return IN_META_INF_EXTERNS.matches(path) || EXTERNS_SOURCES.matches(path);
+        }
+
+        @Override
+        public String toString() {
+            return "externs to pass to closure";
+        }
+    };
+
+    /**
+     * JS files that closure should accept as input to bundle/compile.
+     */
+    private static final PathMatcher PLAIN_JS_SOURCES = new PathMatcher() {
+        @Override
+        public boolean matches(Path path) {
+            if (IN_META_INF.matches(path) && !IN_META_INF_EXTERNS.matches(path)) {
+                return false;
+            }
+            if (IN_PUBLIC.matches(path)) {
+                return false;
+            }
+            return JS_SOURCES.matches(path) && !NATIVE_JS_SOURCES.matches(path) && !EXTERNS.matches(path);
+        }
+
+        @Override
+        public String toString() {
+            return "Only non-native JS sources";
+        }
+    };
 
     public ClosureTask(Dependency dep, BuildContext buildContext) {
         super(dep, buildContext);
@@ -17,6 +84,40 @@ public class ClosureTask extends TaskInput {
     @Override
     public void process() {
         TaskOutput self = input(dep, OutputTypes.TRANSPILED_JS);
-        TaskOutput deps = input(dep.getDependencies(), OutputTypes.TRANSPILED_JS);
+        //TaskOutput transpiled = input(dep.getDependencies(), OutputTypes.TRANSPILED_JS);
+
+/*        List<Dependency> allDependencies = getAllDependencies();
+        TaskOutput transpiled = input(allDependencies, OutputTypes.TRANSPILED_JS);
+        TaskOutput sources = input(allDependencies, OutputTypes.TRANSPILED_JS);
+
+        Stream<FileEntry> asStream = Stream.concat(
+                transpiled.files().stream(),
+                sources.files().stream()
+        );
+
+        asStream.forEach(f -> {
+            System.out.println();
+        });*/
+
+
+
+
+    }
+
+
+    private List<Dependency> getAllDependencies() {
+        Set<Dependency> processed = new HashSet<>();
+        List<Dependency> result = new ArrayList<>();
+        Queue<Dependency> queue = new LinkedList<>();
+        queue.add(dep);
+        while (!queue.isEmpty()) {
+            Dependency current = queue.poll();
+            if (!processed.contains(current)) {
+                result.add(current);
+                processed.add(current);
+                queue.addAll(current.getDependencies());
+            }
+        }
+        return result;
     }
 }
--- plugin/src/main/java/org/example/task/TaskInput.java
+++ plugin/src/main/java/org/example/task/TaskInput.java
@@ -3,6 +3,9 @@ package org.example.task;
 import org.example.context.BuildContext;
 import org.example.model.Dependency;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.PathMatcher;
 import java.util.List;
@@ -35,30 +38,49 @@ public abstract class TaskInput {
                 .runAsync(doProcess(), buildContext.executor())
                 .thenApplyAsync(v -> outputPath(), buildContext.executor())
                 .exceptionally(ex -> {
+                    markModuleFailed();
+                    System.out.println("Failed to process task " + key() + ": " + ex.getMessage());
                     throw new CompletionException("Task " + key() + " failed", ex);
                 });
     }
 
-    private void beforeProcess() {
-        insureOutputDirectoryExists();
-        outputPath().toFile().mkdirs();
-        if (!outputPath().toFile().exists()) {
-            throw new RuntimeException(String.format("Unable to create output path %s", outputPath()));
+    private void markModuleFailed() {
+        for(OutputTypes outputType : OutputTypes.values()) {
+            Path markPath = outputPath().getParent().resolve(outputType.getName());
+            System.out.println("Marking " + markPath + " as failed " + markPath.toFile().exists());
+            if (markPath.toFile().exists()) {
+                markFailed(markPath);
+            }
         }
+    }
 
+    private boolean beforeProcess() {
+        if (!outputPath().toFile().exists()) {
+            insureOutputDirectoryExists();
+            outputPath().toFile().mkdirs();
+            markFailed(outputPath());
+        } else {
+            if (hasSuccessMarker(outputPath())) {
+                return false;
+            }
+        }
         System.out.println(String.format("Starting  task %s for %s", getOutputTypes().getName(), dep.key()));
+        return true;
     }
 
     private void afterProcess(double startTime) {
+        markSuccess(outputPath());
         System.out.println(String.format("Completed task %s for %s in %.2f seconds", getOutputTypes().getName(), dep.key(), (System.currentTimeMillis() - startTime) / 1000.0));
     }
 
     private Runnable doProcess() {
         return () -> {
             double startTime = System.currentTimeMillis();
-            beforeProcess();
-            process();
-            afterProcess(startTime);
+            boolean doContinue = beforeProcess();
+            if (doContinue) {
+                process();
+                afterProcess(startTime);
+            }
         };
     }
 
@@ -129,6 +151,55 @@ public abstract class TaskInput {
 
     protected Path outputPath() {
         return buildContext.getOutputDirectory().resolve(dep.key()).resolve(getOutputTypes().getName());
+    }
+
+    private void markFailed(Path output) {
+        Path failedMarker = output.resolve(".failed");
+        if (!Files.exists(failedMarker)) {
+            try {
+                Files.createFile(failedMarker);
+            } catch (IOException e) {
+                throw new RuntimeException("Unable to create failed marker file: " + failedMarker, e);
+            }
+        }
+        Path successMarker = output.resolve(".success");
+        if (Files.exists(successMarker)) {
+            try {
+                Files.delete(successMarker);
+            } catch (IOException e) {
+                throw new RuntimeException("Unable to delete success marker file: " + successMarker, e);
+            }
+        }
+    }
+
+    protected void markSuccess(Path output) {
+        Path failedMarker = output.resolve(".failed");
+        if (Files.exists(failedMarker)) {
+            try {
+                Files.delete(failedMarker);
+            } catch (IOException e) {
+                throw new RuntimeException("Unable to delete failed marker file: " + failedMarker, e);
+            }
+        }
+        Path successMarker = output.resolve(".success");
+        if (!Files.exists(successMarker)) {
+            try {
+                Files.createFile(successMarker);
+            } catch (IOException e) {
+                throw new RuntimeException("Unable to create success marker file: " + successMarker, e);
+            }
+        }
+
+    }
+
+    private boolean hasFailedMarker(Path output) {
+        Path failedMarker = output.resolve(".failed");
+        return Files.exists(failedMarker);
+    }
+
+    protected boolean hasSuccessMarker(Path output) {
+        Path successMarker = output.resolve(".success");
+        return Files.exists(successMarker);
     }
 
 }
