1169c551fb4356137abda08af5f2a915e04b7027
Dmitrii Tikhomirov <chani.liet@gmail.com>
2026-07-30 14:37:39 -0700

    32
    Signed-off-by: Dmitrii Tikhomirov <chani.liet@gmail.com>

--- samples/multi-module/app/src/main/java/example/app/Main.java
+++ samples/multi-module/app/src/main/java/example/app/Main.java
@@ -4,8 +4,10 @@ import elemental2.dom.DomGlobal;
 import example.lib.Greeter;
 import org.treblereel.j2cl.processors.annotations.GWT3EntryPoint;
 
-public class Main {
+/** Multi-module sample entry point. */
+public final class Main {
 
+    /** Application bootstrap. */
     @GWT3EntryPoint
     public void onLoad() {
         Greeter greeter = new Greeter("J2CL");
